d311cc95c660a94683f7f1ea833f7b8f875286d7
Beau <tian.bq@xinanio.com>
2023-11-29 16:26:10 +0800

    split paragraphs and sentences

--- src/util/embedding.ts
+++ src/util/embedding.ts
@@ -1,25 +1,34 @@
 import { isNilEmpty } from "./puref";
 
-const splitTextIntoSentences = (text: string, options: { chunkSize: number }) => {};
+/**
+ * Tokenizes sentences. Suitable for English, Chinese, Japanese, and Korean.
+ * Allows English delimeters in CJK context
+ */
+const sentenceTokenizer = (text: string) => {
+  return text.match(/.+?[.?!]+[\])'"`’”]*(?:\s|$)|.+?[。？！.?!]+?[\])）'"`’”]*(?:\s|$)?|.+/g);
+  // return text.match(/.+?[。？！.?!]+[\])）'"`’”]*(?:\s|$)?|.+/g);
+};
+
+export const splitTextIntoSentences = (text: string) => {
+  const sentenceSplits = sentenceTokenizer(text).map((x) => x.trim());
+  return sentenceSplits;
+};
 
 export const splitTextIntoParagraphs = (
   text: string,
-  paragraphSeparator: string,
   options: { chunkSize?: number } = { chunkSize: 1024 }
 ) => {
   if (isNilEmpty(text)) throw new Error("no text to split");
 
   // get paragraph splits
-  let paragraphSplits: string[] = text.split(paragraphSeparator);
+  let paragraphSplits: string[] = text.split(/[\r\n]?\n[\r\n]?\n/);
   if (options?.chunkSize) {
     let idx = 0;
 
     // merge paragraphs that are too small
     while (idx < paragraphSplits.length) {
       if (idx < paragraphSplits.length - 1 && paragraphSplits[idx].length < options.chunkSize) {
-        paragraphSplits[idx] = [paragraphSplits[idx], paragraphSplits[idx + 1]].join(
-          paragraphSeparator
-        );
+        paragraphSplits[idx] = [paragraphSplits[idx], paragraphSplits[idx + 1]].join("\n");
         paragraphSplits.splice(idx + 1, 1);
       } else {
         idx += 1;
--- test/util/embedding.test.ts
+++ test/util/embedding.test.ts
@@ -1,20 +1,24 @@
 import { expect, test } from "vitest";
-import { splitTextIntoParagraphs } from "../../src/util/embedding";
+import { splitTextIntoParagraphs, splitTextIntoSentences } from "../../src/util/embedding";
 
 test("split paragraph", () => {
   const splits = splitTextIntoParagraphs(
     "This is a paragraph.\n\nThis is another paragraph.",
-    "\n\n",
     {}
   );
   expect(splits).toEqual(["This is a paragraph.", "This is another paragraph."]);
 });
 
 test("splits paragraphs with chunk size", () => {
-  let splits = splitTextIntoParagraphs(
-    "This is a paragraph.\nThis is another paragraph.",
-    "\n",
-    { chunkSize: 100 }
-  );
+  const splits = splitTextIntoParagraphs("This is a paragraph.\nThis is another paragraph.", {
+    chunkSize: 100
+  });
   expect(splits).toEqual(["This is a paragraph.\nThis is another paragraph."]);
+});
+
+test("split sentences", () => {
+  const sentenceSplits = splitTextIntoSentences(
+    "This is a.This is b. This is c! 这是一个句子. 这是另一个句子。这还是一个句子"
+  );
+  expect(sentenceSplits).toHaveLength(5);
 });
